717b0e7cf4e0c8ddaf82160a68e2da1082ef8e85
Gant Laborde <GantMan@gmail.com>
2018-05-11 16:56:21 -0500

    finished exercise

--- machine-learning-ex2/ex2/costFunction.m
+++ machine-learning-ex2/ex2/costFunction.m
@@ -7,7 +7,7 @@ function [J, grad] = costFunction(theta, X, y)
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -20,12 +20,10 @@ grad = zeros(size(theta));
 % Note: grad should have the same dimensions as theta
 %
 
+h_of_x = sigmoid(X * theta)
 
-
-
-
-
-
+J = 1 / m * sum( -1 * y' * log(h_of_x) - (1-y') * log(1 - h_of_x) )
+grad = 1 / m * (X' * (h_of_x - y))
 
 % =============================================================
 
--- machine-learning-ex2/ex2/costFunctionReg.m
+++ machine-learning-ex2/ex2/costFunctionReg.m
@@ -2,12 +2,12 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %COSTFUNCTIONREG Compute cost and gradient for logistic regression with regularization
 %   J = COSTFUNCTIONREG(theta, X, y, lambda) computes the cost of using
 %   theta as the parameter for regularized logistic regression and the
-%   gradient of the cost w.r.t. to the parameters. 
+%   gradient of the cost w.r.t. to the parameters.
 
 % Initialize some useful values
 m = length(y); % number of training examples
 
-% You need to return the following variables correctly 
+% You need to return the following variables correctly
 J = 0;
 grad = zeros(size(theta));
 
@@ -17,10 +17,13 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
+[J, grad] = costFunction(theta, X, y)
 
+% this effectively ignores "theta zero" in the following calculations
+theta_zeroed_first = [0; theta(2:length(theta));]
 
-
-
+J = J + lambda / (2 * m) * sum( theta_zeroed_first .^ 2 )
+grad = grad .+ (lambda / m) * theta_zeroed_first
 
 % =============================================================
 
