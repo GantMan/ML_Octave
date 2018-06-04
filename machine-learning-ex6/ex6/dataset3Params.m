26a37bffb03e915c1f702fb476ff00ebfa77b20c
Gant <GantMan@gmail.com>
2018-06-03 22:22:50 -0500

    ex6 completed

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -2,8 +2,8 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 %DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
 %where you select the optimal (C, sigma) learning parameters to use for SVM
 %with RBF kernel
-%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
-%   sigma. You should complete this function to return the optimal C and 
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and
+%   sigma. You should complete this function to return the optimal C and
 %   sigma based on a cross-validation set.
 %
 
@@ -15,18 +15,48 @@ sigma = 0.3;
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
 %               You can use svmPredict to predict the labels on the cross
-%               validation set. For example, 
+%               validation set. For example,
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
 %
-%  Note: You can compute the prediction error using 
+%  Note: You can compute the prediction error using
 %        mean(double(predictions ~= yval))
 %
 
+fprintf('--------------------------------------------------------------------------------\n')
+fprintf('start searching best [C, sigma] values\n')
+testValues = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+
+for i=1:length(testValues), % Test all possible values for C
+    for j=1:length(testValues), % Test for sigma
+
+      % Use the svmTrain function to get model output for each
+      % possible combination of i,j (C, sigma). Note that in
+      % svmTrain we have to pass in a kernelFunction
 
 
+      testModel = svmTrain(X, y, testValues(i), \
+        @(x1, x2) gaussianKernel(x1,x2,testValues(j)))
 
+      % Now we can call svmPredict to get the prediction vectors
 
+      testPredictions = svmPredict(testModel, Xval)
+
+      % Now we can calculate the prediction error using the above
+      % formula
+
+      crossValPredictErrors(i,j) = mean(double(testPredictions ~= yval))
+    end
+end
+
+[minColumnError, minColumnErrorIndex] = min(crossValPredictErrors)
+[minError, minErrorIndex] = min(minColumnError)
+
+C = testValues(minColumnErrorIndex(minErrorIndex))
+sigma = testValues(minErrorIndex)
+
+fprintf('\nfinish searching.\nBest value [C, sigma] = [%f %f] with prediction error = %f\n\n', C, sigma, minError)
+fprintf('--------------------------------------------------------------------------------\n')
 
 
 % =========================================================================
